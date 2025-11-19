03db36c57648b1ead1a35ff9db6388169181b73c
shafe123 <ethan.h.shafer@gmail.com>
2025-11-19 11:50:46 -0500

    adds sample commands and fixes health check

---
+++ Misc Topics/Containers and Images/commands.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+docker buildx build -t todo_django:latest .
+
+docker image inspect todo_django
+docker image inspect todo_django | jq .[0].RootFS
+
+docker image pull python:3.12-alpine
+docker image inspect python:3.12-alpine | jq .[0].RootFS
+
+mkdir data
+docker container run --rm -v ./data:/data -p 8080:8000 todo_django
+docker container list
+
+docker container exec -it <container_name> /bin/sh
+docker inspect --format "{{json .State.Health }}" <container_name> | jq
